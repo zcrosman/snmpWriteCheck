e2cfdab05e5c9fa1ee657c38858c2926edec36b5
Zach Crosman <21688962+zcrosman@users.noreply.github.com>
2021-10-27 20:13:54 -0500

    Create snmpWriteCheck.sh

---
+++ snmpWriteCheck.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ipFile="/home/kali/Documents/IPs.txt"
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    echo "Use the format - ./snmpWriteCheck <path/to/IPlist>"
+    echo "Example: ./snmpWriteCheck /home/user/Documents/IP.lst"
+    exit 1
+fi
+
+while IFS= read -r line
+do
+	check=$(snmp-check $line -w | grep -E 'permitted|timeout')
+	
+	if [[ $check == *'not'* ]]; then
+		echo "${line}  - SNMP read only"
+	elif [[ $check == *'Wite access permitted'* ]]; then
+		echo "${line}  - SNMP read/write access!"
+	elif [[ $check == *'timeout'* ]]; then
+		echo "${line}  - SNMP timeout"
+	else echo "Error testing SNMP access"
+	fi
+done < $1
+
+
